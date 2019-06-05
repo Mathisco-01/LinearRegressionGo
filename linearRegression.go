8a80bb3bd5b4c32e6552b1a88f9082e3f871db00
Mathis Van Eetvelde <mathis.vaneetvelde@gmail.com>
2019-06-06 00:25:29 +0200

    added gradient descent

--- linearRegression.go
+++ linearRegression.go
@@ -48,8 +48,8 @@ func loadData(fileName string) ([]float64, []float64, int) {
 func main() {
 	X, y, m := loadData("data.txt")
 	theta := []float64{0, 0}
-
-	fmt.Println(cost(X, y, m, theta))
+	ntheta := gradientDescent(X, y, m, theta)
+	fmt.Println("Current theta: ", ntheta, " cost: ", cost(X, y, m, ntheta))
 }
 
 func cost(X []float64, y []float64, m int, theta []float64) float64 {
@@ -63,8 +63,41 @@ func cost(X []float64, y []float64, m int, theta []float64) float64 {
 	return J
 }
 
-func gradientDescent() {
-	//need to add gradient descent
-	//will do later
-	//probably
+func individualCost(X float64, y float64, theta []float64) float64 {
+	hval := X*theta[0] + theta[1]
+	J := math.Pow((hval - y), 2)
+	return J / float64(2)
+}
+
+func individualPredict(X float64, theta []float64) float64 {
+	return X
+}
+
+func gradientDescent(X []float64, y []float64, m int, theta []float64) []float64 {
+	learningRate := .001
+	iterations := 1000
+
+	currentCost := cost(X, y, m, theta)
+	lowestCost := currentCost
+	lowestCostTheta := []float64{0, 0}
+
+	for i := 0; i <= iterations; i++ {
+
+		tempTheta := []float64{0, 0}
+		for j := 0; j <= m; j++ {
+			iCost := individualCost(X[i], y[i], theta)
+			tempTheta[0] = tempTheta[0] + float64(learningRate)*iCost*X[i]
+			tempTheta[1] = tempTheta[1] + float64(learningRate)*iCost
+
+		}
+		theta[0] = tempTheta[0] / float64(m)
+		theta[1] = tempTheta[1] / float64(m)
+		currentCost = cost(X, y, m, theta)
+		if currentCost < lowestCost {
+			lowestCostTheta = theta
+			lowestCost = currentCost
+			break
+		}
+	}
+	return lowestCostTheta
 }
